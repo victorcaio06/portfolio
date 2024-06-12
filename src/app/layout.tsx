fd183e4d737413db6f6362d7bcdb982a184a69d4
Victor Caio <kaioaraujo426@gmail.com>
2024-06-12 18:28:57 -0300

    FIRST COMMIT

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import { JetBrains_Mono } from 'next/font/google';
+import './globals.css';
+
+const jetBrainsMono = JetBrains_Mono({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+  variable: '--font-jetbrainsMono'
+});
+
+export const metadata: Metadata = {
+  title: 'Portfólio Victor Caio',
+  description: 'Portfólio Dev Frontend',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={jetBrainsMono.variable}>{children}</body>
+    </html>
+  );
+}
