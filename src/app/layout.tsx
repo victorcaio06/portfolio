83c098c40dc634f3958228d0bd54d0c928522b56
Victor Caio <kaioaraujo426@gmail.com>
2024-06-21 00:29:19 -0300

    FEAT: adds a header part and transition animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { JetBrains_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
+import PageTransition from '@/components/PageTransition';
+import StairTransition from '@/components/StairTransition';
 
 const jetBrainsMono = JetBrains_Mono({
   subsets: ['latin'],
@@ -23,8 +25,8 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className={jetBrainsMono.variable}>
         <Header />
-
-        {children}
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
